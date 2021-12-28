445041c9f16085ac2c5f87890fe1b24e39339acb
divya9072 <divi9072@gmail.com>
2021-12-28 12:12:56 +0530

    second commit

--- src/Movie.js
+++ src/Movie.js
@@ -1,18 +1,18 @@
-import {React,useState,useEffect} from "react";
+import { React, useState, useEffect } from "react";
 import axios from "axios";
 import Header from "./Header";
 import "./styles.css";
 import { Link } from "react-router-dom";
-import {Button} from '@mui/material';
-import {makeStyles} from '@mui/styles';
+import { Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
-    btnStyle :{
-          borderRadius: 50,
-          fontSize: 60 ,
-          backgroundColor: 'red'
-      }
-    })
+  btnStyle: {
+    fontSize: '17px !important',
+    backgroundColor: '#1A374D !important',
+    borderRadius: '10px !important'
+  }
+})
 
 
 const Search_Api = "https://api.themoviedb.org/3/search/movie?&api_key=0294919b7060e3e3a5be90f5a15e9361&query=";
@@ -42,17 +42,18 @@ let Movie = () => {
     <div>
       <Header />
       <div>
-      <input
-        className='search'
-        type='search'
-        placeholder='Search movie here..'
-        value={searchedItem}
-        onChange={handleOnChange}
-      ></input>
+        <input
+          className='search'
+          type='search'
+          placeholder='Search movie here..'
+          value={searchedItem}
+          onChange={handleOnChange}
+        ></input>
       </div>
       <div className="search--button">
-      <Button className = {classes.btnStyle} variant="contained" onClick={() => {
-        handleOnSubmit()}}>Search</Button>
+        <Button className={classes.btnStyle} variant="contained" onClick={() => {
+          handleOnSubmit()
+        }}>Search</Button>
       </div>
       {movies && (
         <div className='movie-wrapper'>
@@ -60,16 +61,16 @@ let Movie = () => {
           {movies.map((movie) => (
             <div className="inside">
               <div className="">
-              <Link style={{ textDecoration: 'none' }}
+                <Link style={{ textDecoration: 'none' }}
 
-                to={`/Moviecard/${movie.id}`}
-                key={movie.id} >
-                <img src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} className="listimg"></img></Link>
-                </div>
-                <div className="insidediv">
-              <p className='movie-list'><strong>Name: </strong>{movie.title}</p>
-              <p className='movie-list'><strong>Overview: </strong>{movie.overview}</p>
-              <p className='movie-list'><strong>Release_date: </strong>{movie.release_date}</p>
+                  to={`/Moviecard/${movie.id}`}
+                  key={movie.id} >
+                  <img src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} className="listimg"></img></Link>
+              </div>
+              <div className="insidediv">
+                <p className='movie-list'><strong>Name: </strong>{movie.title}</p>
+                <p className='movie-list'><strong>Overview: </strong>{movie.overview}</p>
+                <p className='movie-list'><strong>Release_date: </strong>{movie.release_date}</p>
               </div>
             </div>
 
